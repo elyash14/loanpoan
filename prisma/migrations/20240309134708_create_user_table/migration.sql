0706af3bff2bd7aef7fd46e79ca40b53c85faf54
Elyas <elyas.mosayebi@gmail.com>
2024-03-09 19:57:50 +0330

    fix: initialize the database and prisma

---
+++ prisma/migrations/20240309134708_create_user_table/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('WOONAM', 'MAN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- src/app/dashboard/users/components/UsersList.tsx
+++ src/app/dashboard/users/components/UsersList.tsx
@@ -1,39 +1,48 @@
-'use client';
+"use client";
 
 import RichTable from "@dashboard/components/table/RichTable";
 import { IRichTableSort } from "@dashboard/components/table/interface";
-import { useState } from "react";
+import { User } from "@prisma/client";
+import { FC, useState } from "react";
 
 const elements = [
-    { id: 6, name: 'Carbon', symbol: 'C', mass: 12.011, },
-    { id: 7, name: 'Nitrogen', symbol: 'N', mass: 14.007, },
-    { id: 39, name: 'Yttrium', symbol: 'Y', mass: 88.906, },
-    { id: 56, name: 'Barium', symbol: 'Ba', mass: 137.33, },
-    { id: 58, name: 'Cerium', symbol: 'Ce', mass: 140.12, },
+  { id: 6, name: "Carbon", symbol: "C", mass: 12.011 },
+  { id: 7, name: "Nitrogen", symbol: "N", mass: 14.007 },
+  { id: 39, name: "Yttrium", symbol: "Y", mass: 88.906 },
+  { id: 56, name: "Barium", symbol: "Ba", mass: 137.33 },
+  { id: 58, name: "Cerium", symbol: "Ce", mass: 140.12 },
 ];
 
 const data = {
-    headers: [
-        { name: "id", sortable: true },
-        { name: "Element name", sortable: true },
-        { name: "Symbol" },
-        { name: "Atomic mass" },
-    ],
-    rows: elements,
-}
+  headers: [
+    { name: "id", sortable: true },
+    { name: "Element name", sortable: true },
+    { name: "Symbol" },
+    { name: "Atomic mass" },
+  ],
+  rows: elements,
+};
 
-const UsersList = () => {
-    const [sort, setSort] = useState<IRichTableSort>({ column: "id", dir: "desc" })
-    const handleSort = (sortable: IRichTableSort) => {
-        setSort(sortable);
-    }
+const UsersList: FC<{ user: User }> = ({ user }) => {
+  const [sort, setSort] = useState<IRichTableSort>({
+    column: "id",
+    dir: "desc",
+  });
+  const handleSort = (sortable: IRichTableSort) => {
+    setSort(sortable);
+  };
 
-    return <RichTable
+  return (
+    <>
+      <h1>{user.firstName}</h1>
+      <RichTable
         data={data}
         hasRowSelector
         sort={sort}
         handleSort={handleSort}
-    />
-}
+      />
+    </>
+  );
+};
 
 export default UsersList;
